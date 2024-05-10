6e21e43c8add0af5f3becf4fc2e8f3d0158d8115
Anthony <aiszler29@gmail.com>
2024-05-09 20:02:59 -0700

    add some struct data and test stuff

--- main.go
+++ main.go
@@ -5,11 +5,43 @@ import (
 	"rpgGo/rpgGo"
 )
 
+func pickClass(class string) (string, rpgGo.Class) {
+	var classPicked string
+	var classPickedClass rpgGo.Class
+	switch class {
+	case "Wizard":
+		classPicked = rpgGo.WizardClass.Name
+	case "Warrior":
+		classPicked = rpgGo.WarriorClass.Name
+	default:
+		classPicked = rpgGo.RogueClass.Name
+	}
+
+	return classPicked, classPickedClass
+}
+
+func BuildCharacter(cn string, class string, base rpgGo.Character) {
+	classPicked, classInfo := pickClass(class)
+	fmt.Println(classPicked)
+
+	character := rpgGo.Character{
+		Name:  cn,
+		Level: 1,
+		Class: classPicked,
+		HP:    base.HP + classInfo.HPchange,
+		Atk:   base.Atk + classInfo.Atkchange,
+		Def:   base.Def + classInfo.Defchange,
+		Int:   base.Int + classInfo.Intchange,
+		Wis:   base.Wis + classInfo.Wischange,
+		Cha:   base.Cha + classInfo.Chachange,
+	}
+	fmt.Println(character)
+}
+
 func main() {
 
+	base := rpgGo.Character{}
+	BuildCharacter("Tony", "Warrior", base)
 	fmt.Println("Hello, 世界")
 
-	for _, character := range rpgGo.Characters {
-		fmt.Printf("Name: %s, Class: %s, Level: %d, HP: %d\n", character.Name, character.Class, character.Level, character.HP)
-	}
 }
--- rpgGo/character.go
+++ rpgGo/character.go
@@ -8,14 +8,70 @@ type Character struct {
 	Atk   int
 	Def   int
 	Int   int
+	Dex   int
 	Wis   int
 	Cha   int
 
 	// Add more fields as needed
 }
 
-// Define some sample data
-var Characters = []Character{
-	{"Alice", "open", 10, 100, 4, 4, 4, 4, 4},
-	// Add more characters as needed
+var baseCharacter = Character{
+	Name:  "",
+	Class: "",
+	Level: 1,
+	HP:    10,
+	Atk:   4,
+	Def:   4,
+	Int:   4,
+	Dex:   4,
+	Wis:   4,
+	Cha:   4,
+
+	// Add more fields as needed
 }
+
+type Class struct {
+	Name      string
+	HPchange  int
+	Atkchange int
+	Defchange int
+	Intchange int
+	Dexchange int
+	Wischange int
+	Chachange int
+}
+
+var WizardClass = Class{
+	Name:      "Wizard",
+	HPchange:  +2,
+	Atkchange: -1,
+	Defchange: -2,
+	Intchange: +5,
+	Dexchange: +1,
+	Wischange: +2,
+	Chachange: +0,
+}
+
+var WarriorClass = Class{
+	Name:      "Warrior",
+	HPchange:  +6,
+	Atkchange: +5,
+	Defchange: +3,
+	Intchange: -1,
+	Dexchange: 0,
+	Wischange: -1,
+	Chachange: -1,
+}
+
+var RogueClass = Class{
+	Name:      "Rogue",
+	HPchange:  0,
+	Atkchange: +1,
+	Defchange: -2,
+	Intchange: -1,
+	Dexchange: +4,
+	Wischange: +1,
+	Chachange: +3,
+}
+
+// Define some sample data
